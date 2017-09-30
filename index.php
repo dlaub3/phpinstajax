763eed59f3e19959df372e946fdb932d74ff8f41
Daniel Laubacher <dlaubacher3@gmail.com>
2017-09-30 13:55:05 -0400

    add index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <main>
+        <form action="instagram.php" method="get">
+            <input type="text" name="search" value=""><br />
+            <input type="submit" name="search" value="Search"><br />
+        </form>
+    </main>
+</body>
+</html>
